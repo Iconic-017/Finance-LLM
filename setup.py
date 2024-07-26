db68ba0bc0dce370047806a71733696f43abcf59
ankitsihag2003 <157290589+ankitsihag2003@users.noreply.github.com>
2024-07-26 12:22:22 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+  name = "medical-chatbot",
+  version= "0.0.0",
+  author="ankitsihag2003",
+  author_email="ankitsihag2003@gmail.com",
+  packages=find_packages(),
+  install_requires = []
+)
